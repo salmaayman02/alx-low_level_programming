ab2d4b13aaacec3638fee1585e20223c32d57a66
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-20 16:28:21 -0700

    cfile

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  * Description : program prints with puts function
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0)
 }
