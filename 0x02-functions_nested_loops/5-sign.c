df42e6d03909c88bcdf525802b929ebbf9b77522
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-21 12:28:56 -0700

    ss

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,13 +13,19 @@ int print_sign(int n)
 	int r;
 
 	if (n > 0)
+	{
 		r = 1;
 		_putchar('+');
+	}
 	else if (n < 0)
+	{
 		r = -1;
 		_putchar('-');
+	}
 	else
+	{
 		r = 0;
 		_putchar('0');
+	}
 	return (r);
 }
