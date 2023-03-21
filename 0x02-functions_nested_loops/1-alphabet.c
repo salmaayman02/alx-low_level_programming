64ec9856ab3934d811a08d3cc1ef3352e6508186
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-21 10:02:29 -0700

    dd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints "alphabet "
+ *
+ * Description : program print
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar('c');
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
