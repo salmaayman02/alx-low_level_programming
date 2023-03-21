b368826c3aebd049e76b1f8978d7b417bc7f892a
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-21 13:26:34 -0700

    ss

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,18 +16,18 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if (a <= 1 && b <= 9 || a == 2 && b <= 3)
+			if ((a <= 1 && b <= 9) || (a == 2 && b <= 3))
 			{
 				for (c = 0; c <= 5; c++)
 				{
 					for (d = 0; d <= 9; d++)
 					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(':');
-						putchar(c + '0');
-						putchar(d + '0');
-						putchar('\n');
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
 					}
 				}
 			}
