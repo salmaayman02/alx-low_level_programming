69cce48ff53e88eda1d05168a96bb7f2343abba6
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-23 07:49:01 -0700

    dd

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - numbers
+ *
+ * Description : print number from 1to14
+ *
+ * Return: no return value
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
