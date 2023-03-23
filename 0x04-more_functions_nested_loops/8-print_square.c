7fd7851623ca6f55795bdc02726477bb142b4b8c
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-23 11:56:30 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - print squre
+ * @size: number of #
+ * Description : print squre
+ *
+ * Return: no return value
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
