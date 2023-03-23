617e611b42972134164dd8d8f11b73046e285706
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-23 05:52:02 -0700

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multyple
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Description : multyple of two digit
+ *
+ * Return: rturn boolean value 1 or 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
