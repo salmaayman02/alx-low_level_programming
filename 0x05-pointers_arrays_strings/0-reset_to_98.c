ca6e9727de74816858f0a24baadcd46a3cbb7174
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-27 16:43:51 -0700

    dd

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - rest n to 98
+ * @n: pointer to integer
+ *
+ * Descreption: rest n to 98
+ *
+ * Return: no return value
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
