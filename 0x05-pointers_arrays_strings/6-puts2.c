c7de2119d2186d5fb27273d07926c5f6ddda4691
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-03-28 11:02:01 -0700

    ss

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - reverse
+ * @str: string
+ *
+ * Descreption: reverse string
+ *
+ * Return: no return value
+*/
+
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		i++;
+
+	}
+}
