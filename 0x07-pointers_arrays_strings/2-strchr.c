93b25627faf095f17dd2459f959f89e8f9f4667e
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-04-03 15:24:47 -0700

    ss

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,6 +11,7 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+	char *p = NULL;
 
 	while (s[i] != '\0')
 	{
@@ -19,5 +20,5 @@ char *_strchr(char *s, char c)
 		i++;
 	}
 
-	return ('NULL');
+	return (p);
 }
