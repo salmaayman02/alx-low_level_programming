8e43cb8f6024f2a68002543fdc84fffb908180de
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-04-03 14:49:00 -0700

    ss

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _memcpy - copy string in an other
+ * @dest: pointer to string
+ * @src: poiter string
+ * @n: size byte
+ *
+ * Descreption: copy string in an other
+ *
+ * Return: return string
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+}
