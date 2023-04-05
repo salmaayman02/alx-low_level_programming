1ccfa9c90c80a12b419c69bb2d7581462ef9e53e
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-04-05 16:04:27 -0700

    ss

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,12 +11,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	i++;
-	if (s[i] != '\0')
-		_puts_recursion(s[i]);
-	else
+	if (*s == '\0')
 		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
