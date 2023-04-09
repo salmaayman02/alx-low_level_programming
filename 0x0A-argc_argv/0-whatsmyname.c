51ab06a51561bba6c889bc5745754df6b48b634f
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-04-08 18:39:08 -0700

    ss

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: size of argv
+ * @argv: pointer of string
+ *
+ * Description: print file name
+ *
+ * Return: integr value
+*/
+
+
+int main(int argc, char *argv[])
+{
+	printf("%s/n", argv[0]);
+
+	return (0);
+}
