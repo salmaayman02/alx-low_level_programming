06dcb7aee6c4c3a27fa0d495cc3d170d4d9f6890
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-04-10 15:08:33 -0700

    ss

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: size of argv
+ * @argv: pointer of string
+ *
+ * Description: print file name
+ *
+ * Return: integr value
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (argv[i] < '0' || argv[i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum = sum + atoi(argv[argc]);
+			}
+		}
+	}
+	return (0);
+}
