637d64869cb5dd2f9cfac83c8f601ac676e99326
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-04-12 12:59:43 -0700

    ss

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL && s2 == NULL)
-		return ("");
+		return (0);
 	else if (s1 == NULL)
 		return (s2);
 	else if (s2 == NULL)
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 	ptr = malloc(size + 1);
 
 	if (ptr == 0)
-		return (NULL);
+		return (0)
 
 	else
 	{
