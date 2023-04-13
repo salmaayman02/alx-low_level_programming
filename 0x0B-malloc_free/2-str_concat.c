3042b1e06cd7c4c0d51a7172ef0222fa8e224840
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-04-12 19:44:13 -0700

    ss

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,6 @@ char *str_concat(char *s1, char *s2)
 	int l1 = 0, l2 = 0, size, i, j;
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
-	{
-		s1 = '\0';
-		s2 = '\0';
-	}
 	if (s1 == NULL)
 		s1 = '\0';
 	if (s2 == NULL)
