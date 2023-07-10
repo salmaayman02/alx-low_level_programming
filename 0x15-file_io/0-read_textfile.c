64a2363cbf47e7ac6ae97754f15debbd1f98a4b3
salmaayman02 <salmaayman.mokhtar@gmail.com>
2023-07-10 13:48:07 -0700

    ss

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,33 +13,35 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    int fd;
-    ssize_t n_read, n_written;
+	char *buf;
+	int fd;
+	ssize_t n_read, n_written;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (!buf) {
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
 
-    n_read = read(fd, buf, letters);
-    if (n_read == -1) {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    n_written = write(STDOUT_FILENO, buf, n_read);
-    free(buf);
-    close(fd);
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	free(buf);
+	close(fd);
 
-    if (n_written != n_read)
-        return (0);
+	if (n_written != n_read)
+		return (0);
 
-    return (n_written);
+	return (n_written);
 }
